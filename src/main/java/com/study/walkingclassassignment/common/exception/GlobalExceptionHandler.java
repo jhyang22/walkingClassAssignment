6fbd7d3fab6c8f19f089e940e1d4ee79c930fa5a
jhyang22 <jaeho9229@gmail.com>
2025-05-01 20:05:36 +0900

    chore : 오타 수정

--- src/main/java/com/study/walkingclassassignment/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/study/walkingclassassignment/common/exception/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(CustomException.class)
 	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
 		log.error("CustomException: {}", e.getMessage());
-		ErrorCode errorCode = e.getErrorCode;
+		ErrorCode errorCode = e.getErrorCode();
 		ErrorResponse response = ErrorResponse.of(errorCode, e.getMessage());
 		return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
 	}
