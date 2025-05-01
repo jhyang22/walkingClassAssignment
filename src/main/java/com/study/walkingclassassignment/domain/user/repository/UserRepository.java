7c661c7bc810bb71f81bd18e2ca34a515c22f7e5
jhyang22 <jaeho9229@gmail.com>
2025-05-01 20:02:18 +0900

    feat : UserRepository 예외처리

--- src/main/java/com/study/walkingclassassignment/domain/user/repository/UserRepository.java
+++ src/main/java/com/study/walkingclassassignment/domain/user/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.study.walkingclassassignment.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.study.walkingclassassignment.common.exception.CustomException;
+import com.study.walkingclassassignment.common.exception.ErrorCode;
 import com.study.walkingclassassignment.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	default User findByIdOrElseThrow(Long id) {
+		return findById(id).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+	}
 }
