4e93a51a3fb93ba70ffa85e610e2238fcf65a95f
jhyang22 <jaeho9229@gmail.com>
2025-05-01 16:27:30 +0900

    chore : 과제 생성

---
+++ src/main/java/com/study/walkingclassassignment/WalkingClassAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.study.walkingclassassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkingClassAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkingClassAssignmentApplication.class, args);
+	}
+
+}
