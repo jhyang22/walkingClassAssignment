91536e558bb8c96e79729e941ff4d0786c85c39d
jhyang22 <jaeho9229@gmail.com>
2025-05-07 19:16:19 +0900

    refactor : URL 수정

--- src/main/java/com/study/walkingclassassignment/domain/user/controller/UserController.java
+++ src/main/java/com/study/walkingclassassignment/domain/user/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.walkingclassassignment.common.filter.Const;
@@ -17,14 +16,13 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
 	private final UserService userService;
 
-	@PostMapping("/signin")
-	public ResponseEntity<UserResponseDto> signin() {
+	@PostMapping("/signup")
+	public ResponseEntity<UserResponseDto> signup() {
 
 		UserResponseDto createdUser = userService.createUser();
 
